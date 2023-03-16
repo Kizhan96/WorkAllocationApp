f7cfc26e5f39c26115eb1454dfa184b4d975079e
Нажмутдин Вагабов <54717479+Kizhan96@users.noreply.github.com>
2023-03-16 11:11:02 +0300

    Create main.cpp

---
+++ ForecastWeatherClienServerApp/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+#include "MyChatSrv.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    myserver Server;
+    Server.startServer();
+
+    return a.exec();
+}
